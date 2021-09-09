e77a771d206dba53c18a2ec3f783976203240f84
Djang <django.heimgartner@gmail.com>
2021-09-09 19:08:07 +0200

    Set static and media URL's

--- main/urls.py
+++ main/urls.py
@@ -20,4 +20,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    )
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
