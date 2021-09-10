051c19d91053c737b7b70d9d365709fa6932216d
Djang <django.heimgartner@gmail.com>
2021-09-10 12:51:10 +0200

    Set first API url to connect to

--- discussions/urls.py
+++ discussions/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'discussions'
 
 urlpatterns = [
     path('', views.discussions, name='discussion'),
+
+    # REST API Url's
+    path('list', views.topicList, name='topicList'),
 ]
--- main/urls.py
+++ main/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-    path('discussions', include('discussions.urls')),
+    path('discussions/', include('discussions.urls', namespace='discussions')),
     path('ideas', include('ideas.urls')),
 ]
 
