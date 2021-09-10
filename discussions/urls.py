b0e72db16b42380ce69378b29ada44e58a8555cd
Djang <django.heimgartner@gmail.com>
2021-09-10 14:39:59 +0200

    Set CRUD functionality trough REST for discussions

--- discussions/urls.py
+++ discussions/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('', views.discussions, name='discussion'),
 
     # REST API Url's
-    path('list', views.topicList, name='topicList'),
+    path('list', views.topic_list, name='topic_list'),
+    path('details/<str:pk>/', views.topic_details, name='topic_details'),
+    path('create/', views.topic_create, name='topic_create'),
+    path('update/<str:pk>/', views.topic_update, name='topic_update'),
+    path('delete/<str:pk>/', views.topic_delete, name='topic_delete'),
 ]
--- discussions/views.py
+++ discussions/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import Topic, Comment
 
 from rest_framework.decorators import api_view
@@ -7,12 +8,15 @@ from .serializers import CommentSerializer, TopicSerializer
 
 
 def discussions(request):
-    """ A view to return the discussions page """
+    """
+    A view to return the 
+    discussions page.
+    """
     return render(request, "discussions/discussion.html")
 
 
 @api_view(['GET'])
-def topicList(request):
+def topic_list(request):
     """
     REST framework for Django to serialize
     & return all topics in list form.
@@ -21,6 +25,61 @@ def topicList(request):
     serializer = TopicSerializer(topics, many=True)
 
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def topic_details(request, pk):
+    """
+    REST framework for Django to serialize
+    & return a single topic details.
+    """
+    topic = Topic.objects.get(id=pk)
+    serializer = TopicSerializer(topic, many=False)
+
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def topic_create(request):
+    """
+    REST framework for Django to serialize
+    & create a single topic.
+    """
+    serializer = TopicSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def topic_update(request, pk):
+    """
+    REST framework for Django to serialize
+    & update a single topic details.
+    """
+    topic = Topic.objects.get(id=pk)
+    serializer = TopicSerializer(instance=topic, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def topic_delete(request, pk):
+    """
+    REST framework for Django to serialize
+    & delete a single topic.
+    """
+    topic = Topic.objects.get(topic_id=pk)
+    topic.delete()
+
+    messages.warning(request,('The topic is removed'))
+
+    return Response('The topic is removed')
 
 
 @api_view(['GET'])
