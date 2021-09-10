7ec017a52b617abc3c3d7428d8fc2d500c0f5812
Djang <django.heimgartner@gmail.com>
2021-09-10 10:30:18 +0200

    Set: topic, vote & comment models

--- discussions/models.py
+++ discussions/models.py
@@ -1,3 +1,32 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    """
+    A model for the topics
+    All comments belong to a topic.
+    """
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=35)
+    description = models.TextField()
+    placed = models.BooleanField()
+    datetime = models.DateTimeField()
+    voting = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='topic_votes')
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    """
+    A model for the comments
+    to be collected by the topic.
+    """
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField(max_length=250, blank=True, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.content
