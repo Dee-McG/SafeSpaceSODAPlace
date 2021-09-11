9ae093c556edf5eb47efa72c8cadef9dca5097f4
Daisy McGirr <mcgirr.daisy@gmail.com>
2021-09-11 12:51:02 +0100

    Create profile model admin and form

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    """ User Pforile admin """
+    list_display = (
+        'user',
+        'name',
+        'organisation',
+        'job_title',
+    )
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
