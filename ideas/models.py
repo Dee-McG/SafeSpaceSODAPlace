1809a9bcb38fcc343adf62fc19a4bb4440382176
Djang <django.heimgartner@gmail.com>
2021-09-11 21:22:39 +0200

    Set the idea models

--- ideas/models.py
+++ ideas/models.py
@@ -1,17 +1,31 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
+class Board(models.Model):
+    """
+    A model to open and close
+    the idears board.
+    """
+    id_code = models.CharField(max_length=15)
+    date = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    closed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.id_code
+
+
 class Idea(models.Model):
     """
-    A model to save and display chat messages
+    A model to the board items
+    add, save and display chat messages.
     """
-    idea = models.CharField(max_length=80,
-                            null=False, blank=False)
-    idea_message = models.CharField(max_length=2500,
-                               null=False, blank=False)
-
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    closed = models.BooleanField(default=False)
+    idea_message = models.CharField(max_length=250, null=False, blank=False)
 
     def __str__(self):
         return self.idea_message
