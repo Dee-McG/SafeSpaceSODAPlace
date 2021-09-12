e930cc2f7e513d7d7ce19bbb7be8efb5875b827b
Djang <django.heimgartner@gmail.com>
2021-09-12 23:00:52 +0200

    Update discussion model to make more sense

--- discussions/models.py
+++ discussions/models.py
@@ -9,10 +9,8 @@ class Topic(models.Model):
     """
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=120)
-    description = models.TextField()
     placed = models.BooleanField()
-    datetime = models.DateTimeField()
-    voting = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='topic_votes')
+    datetime = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
