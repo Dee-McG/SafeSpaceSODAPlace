f290815ce1c4c74ae6902db864c511ec612f7171
Daisy McGirr <mcgirr.daisy@gmail.com>
2021-09-13 11:35:22 +0100

    Add env.py for dev

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
 import os
 
 os.environ.setdefault('DEVELOPMENT', 'True')
-os.environ.setdefault('SECRET_KEY', '3r1xk-+gfn2x3(z09in^i=00(z5fk+x(3tp0j5%3rd84b_ud7n')
+os.environ.setdefault('SECRET_KEY', 'FORTESTENV')
