6e246c3a35486158f14e6a47a77541377218afe6
Djang <django.heimgartner@gmail.com>
2021-09-13 11:39:18 +0200

    Refactoring & cleanup discussions serializer

--- discussions/urls.py
+++ discussions/urls.py
@@ -7,9 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
 
     # REST API Url's
-    path('list', views.topic_list, name='topic_list'),
-    path('details/<str:pk>/', views.topic_details, name='topic_details'),
-    path('create/', views.topic_create, name='topic_create'),
-    path('update/<str:pk>/', views.topic_update, name='topic_update'),
-    path('delete/<str:pk>/', views.topic_delete, name='topic_delete'),
+    path('create/', views.comment_create, name='comment_create'),
 ]
